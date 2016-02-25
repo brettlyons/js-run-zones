cb2d5f47b78ffd307c1e90a98171c052a2702edc
Brett Lyons <blyons@students.naropa.edu>
2016-02-24 19:40:17 -0700

    Set up .gitignore for package-json, added simple node server for testing

---
+++ server.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var url = require('url');
+var staticServer = require('node-static');
+// require('dotenv').load()
+//var db = require('monk')(process.env.MONGOLAB_URI);
+//var db = require('monk')('localhost/tasksdb');
+//var tasks = db.get('tasks');
+
+var port = process.env.PORT || 3000;
+var file = new(staticServer.Server)();
+
+var server = http.createServer(function (req, res) {
+  var urlKeys = url.parse(req.url, true);
+  if(req.method != 'GET'){
+    return res.end('Send me a GET\n');
+  }
+  console.log(urlKeys.pathname);
+  console.log(req.url);
+  // if (urlKeys.pathname == '/api/tasks'){
+  //   tasks.find({}, function(err, tasks) {
+  //     res.writeHead(200, {'Content-Type':'application/json'});
+  //     res.end(JSON.stringify(tasks)); // tasks is an array here.
+  //   });
+  // }
+  if (urlKeys.pathname == "/api") {
+    console.log(req.headers);
+    console.log(req.authorization);
+  }
+  else {
+    file.serve(req, res);
+  }
+  // if(req.method == "POST") {
+  //   tasks.insert({
+  //     //put new task description into db . . .
+  //   });
+  // }
+});
+
+server.listen(port);
